ab738955db3a1de674dcf30cdb2f80b077fd6e73
Burt Beckwith <burt@burtbeckwith.com>
2010-08-22 21:22:10 -0400

    removed jars from lib and switched to use Ivy since the plugin is 1.2.2+

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,38 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.class.dir = 'target/classes'
+grails.project.test.class.dir = 'target/test-classes'
+grails.project.test.reports.dir = 'target/test-reports'
+
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	inherits 'global'
 
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-    }
+	log 'warn'
+
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+
+		mavenRepo 'http://download.java.net/maven/2/'
+		mavenCentral()
+	}
+
+	dependencies {
+		runtime('com.enigmastation:ci-bayes:1.0.8') {
+			transitive = false
+//			excludes 'javax.transaction',
+//			         'org.hibernate',
+//			         'hsqldb',
+//			         'org.testng',
+//			         'org.springframework',
+//			         'org.aspectj',
+//			         'org.apache.ant',
+//			         'rome',
+//			         'nekohtml',
+//			         'javatar'
+		}
+		runtime('org.javolution:javolution:5.2.6') {
+			transitive = false
+		}
+	}
 }
