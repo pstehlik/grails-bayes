c091205eda591745424b16402b3297bef2e2dfba
pstehlik <philip.stehlik@gmail.com>
2010-12-04 16:46:00 -0800

    up to 0.4

--- BayesGrailsPlugin.groovy
+++ BayesGrailsPlugin.groovy
@@ -18,9 +18,9 @@
  */
 class BayesGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.2 > *"
+    def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
